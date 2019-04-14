34f3d1f3caba306f4e9a0839ac30e0e1992ef02c
Ricardo Salveti <ricardo@foundries.io>
2019-04-13 21:59:39 -0300

    Random: skip tests if seed/generate are not supported
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- rng.cc
+++ rng.cc
@@ -27,63 +27,118 @@ namespace test {
 TEST_F(ReadOnlySessionTest, SeedRandom) {
   // Additional seed data. Not actually particularly random.
   CK_BYTE seed[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  CK_RV rv;
   if (g_token_flags & CKF_RNG) {
     // Although the token has an RNG, it may or may not support seeding.
-    CK_RV rv = g_fns->C_SeedRandom(session_, seed, sizeof(seed));
+    rv = g_fns->C_SeedRandom(session_, seed, sizeof(seed));
+    if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+      TEST_SKIPPED("SeedRandom not supported");
+      return;
+    }
     EXPECT_TRUE(rv == CKR_OK || rv == CKR_RANDOM_SEED_NOT_SUPPORTED);
   } else {
-    EXPECT_CKR(CKR_RANDOM_NO_RNG, g_fns->C_SeedRandom(session_, seed, sizeof(seed)));
+    rv = g_fns->C_SeedRandom(session_, seed, sizeof(seed));
+    if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+      TEST_SKIPPED("SeedRandom not supported");
+      return;
+    }
+    EXPECT_CKR(CKR_RANDOM_NO_RNG, rv);
   }
 }
 
 TEST(RNG, SeedRandomNoInit) {
   CK_BYTE seed[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  EXPECT_CKR(CKR_CRYPTOKI_NOT_INITIALIZED, g_fns->C_SeedRandom(INVALID_SLOT_ID, seed, sizeof(seed)));
+  CK_RV rv = g_fns->C_SeedRandom(INVALID_SLOT_ID, seed, sizeof(seed));
+  if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+    TEST_SKIPPED("SeedRandom not supported");
+    return;
+  }
+  EXPECT_CKR(CKR_CRYPTOKI_NOT_INITIALIZED, rv);
 }
 
 TEST_F(ReadOnlySessionTest, SeedRandomBadArguments) {
-  EXPECT_CKR(CKR_ARGUMENTS_BAD, g_fns->C_SeedRandom(session_, nullptr, 1));
+  CK_RV rv = g_fns->C_SeedRandom(session_, nullptr, 1);
+  if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+    TEST_SKIPPED("SeedRandom not supported");
+    return;
+  }
+  EXPECT_CKR(CKR_ARGUMENTS_BAD, rv);
 }
 
 TEST_F(PKCS11Test, SeedRandomNoSession) {
   CK_BYTE seed[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  EXPECT_CKR(CKR_SESSION_HANDLE_INVALID, g_fns->C_SeedRandom(INVALID_SLOT_ID, seed, sizeof(seed)));
+  CK_RV rv = g_fns->C_SeedRandom(INVALID_SLOT_ID, seed, sizeof(seed));
+  if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+    TEST_SKIPPED("SeedRandom not supported");
+    return;
+  }
+  EXPECT_CKR(CKR_SESSION_HANDLE_INVALID, rv);
 }
 
 TEST_F(ReadOnlySessionTest, GenerateRandom) {
   CK_BYTE data[1024];
+  CK_RV rv;
   if (g_token_flags & CKF_RNG) {
-    EXPECT_CKR_OK(g_fns->C_GenerateRandom(session_, data, sizeof(data)));
+    rv = g_fns->C_GenerateRandom(session_, data, sizeof(data));
+    if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+      TEST_SKIPPED("GenerateRandom not supported");
+      return;
+    }
+    EXPECT_CKR_OK(rv);
     EXPECT_CKR_OK(g_fns->C_GenerateRandom(session_, data, 10));
     EXPECT_CKR_OK(g_fns->C_GenerateRandom(session_, data, 100));
     CK_BYTE data2[100];
     EXPECT_CKR_OK(g_fns->C_GenerateRandom(session_, data2, 100));
     EXPECT_NE(0, memcmp(data, data2, 100));
   } else {
-    EXPECT_CKR(CKR_RANDOM_NO_RNG, g_fns->C_GenerateRandom(session_, data, sizeof(data)));
+    rv = g_fns->C_GenerateRandom(session_, data, sizeof(data));
+    if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+      TEST_SKIPPED("GenerateRandom not supported");
+      return;
+    }
+    EXPECT_CKR(CKR_RANDOM_NO_RNG, rv);
   }
 }
 
 TEST(RNG, GenerateRandomNoInit) {
   CK_BYTE data[8];
-  EXPECT_CKR(CKR_CRYPTOKI_NOT_INITIALIZED, g_fns->C_GenerateRandom(INVALID_SLOT_ID, data, sizeof(data)));
+  CK_RV rv = g_fns->C_GenerateRandom(INVALID_SLOT_ID, data, sizeof(data));
+  if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+    TEST_SKIPPED("GenerateRandom not supported");
+    return;
+  }
+  EXPECT_CKR(CKR_CRYPTOKI_NOT_INITIALIZED, rv);
 }
 
 TEST_F(ReadOnlySessionTest, GenerateRandomBadArguments) {
-  EXPECT_CKR(CKR_ARGUMENTS_BAD, g_fns->C_GenerateRandom(session_, nullptr, 1));
+  CK_RV rv = g_fns->C_GenerateRandom(session_, nullptr, 1);
+  if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+    TEST_SKIPPED("GenerateRandom not supported");
+    return;
+  }
+  EXPECT_CKR(CKR_ARGUMENTS_BAD, rv);
 }
 
 TEST_F(ReadOnlySessionTest, GenerateRandomNone) {
   CK_BYTE data[64];
   if (g_token_flags & CKF_RNG) {
     CK_RV rv = g_fns->C_GenerateRandom(session_, data, 0);
+    if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+      TEST_SKIPPED("GenerateRandom not supported");
+      return;
+    }
     EXPECT_TRUE(rv == CKR_OK || rv == CKR_ARGUMENTS_BAD) << CK_RV_(rv);
   }
 }
 
 TEST_F(PKCS11Test, GenerateRandomNoSession) {
   CK_BYTE data[16];
-  EXPECT_CKR(CKR_SESSION_HANDLE_INVALID, g_fns->C_GenerateRandom(INVALID_SLOT_ID, data, sizeof(data)));
+  CK_RV rv = g_fns->C_GenerateRandom(INVALID_SLOT_ID, data, sizeof(data));
+  if (rv == CKR_FUNCTION_NOT_SUPPORTED) {
+    TEST_SKIPPED("GenerateRandom not supported");
+    return;
+  }
+  EXPECT_CKR(CKR_SESSION_HANDLE_INVALID, rv);
 }
 
 }  // namespace test
