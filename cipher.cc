f8a7081253977db5ad61c3205445f84cf16352d3
Ricardo Salveti <ricardo@foundries.io>
2019-04-29 21:19:17 -0300

    Cipher: prefer encrypt final for decrypt tests
    EncryptUpdate doesn't release the operation lock.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- cipher.cc
+++ cipher.cc
@@ -447,9 +447,9 @@ TEST_P(SecretKeyTest, DecryptUpdateErrors) {
   CK_RV rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
   SKIP_IF_UNIMPLEMENTED_RV(rv);
   ASSERT_CKR_OK(rv);
-  ASSERT_CKR_OK(g_fns->C_EncryptUpdate(session_,
-                                       plaintext_.get(), kNumBlocks * info_.blocksize,
-                                       ciphertext, &ciphertext_len));
+  ASSERT_CKR_OK(g_fns->C_Encrypt(session_,
+                                 plaintext_.get(), kNumBlocks * info_.blocksize,
+                                 ciphertext, &ciphertext_len));
 
   // Variety of bad arguments to C_DecryptUpdate.  Each error terminates the
   // operation and so need re-initialization.
@@ -542,9 +542,9 @@ TEST_P(SecretKeyTest, DecryptFinalErrors1) {
   CK_RV rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
   SKIP_IF_UNIMPLEMENTED_RV(rv);
   ASSERT_CKR_OK(rv);
-  ASSERT_CKR_OK(g_fns->C_EncryptUpdate(session_,
-                                       plaintext_.get(), kNumBlocks * info_.blocksize,
-                                       ciphertext, &ciphertext_len));
+  ASSERT_CKR_OK(g_fns->C_Encrypt(session_,
+                                 plaintext_.get(), kNumBlocks * info_.blocksize,
+                                 ciphertext, &ciphertext_len));
 
   // Variety of bad arguments to C_DecryptFinal.  Each error terminates the
   // operation and so need re-initialization.
@@ -568,9 +568,9 @@ TEST_P(SecretKeyTest, DecryptFinalErrors2) {
   CK_RV rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
   SKIP_IF_UNIMPLEMENTED_RV(rv);
   ASSERT_CKR_OK(rv);
-  ASSERT_CKR_OK(g_fns->C_EncryptUpdate(session_,
-                                       plaintext_.get(), kNumBlocks * info_.blocksize,
-                                       ciphertext, &ciphertext_len));
+  ASSERT_CKR_OK(g_fns->C_Encrypt(session_,
+                                 plaintext_.get(), kNumBlocks * info_.blocksize,
+                                 ciphertext, &ciphertext_len));
 
   CK_BYTE plaintext[1024];
   CK_BYTE_PTR output = plaintext;
